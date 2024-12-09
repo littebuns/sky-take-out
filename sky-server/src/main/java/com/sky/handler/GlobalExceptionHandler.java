d4b1630415beb41df317e2ce8e487e5486477c2c
xxb <xxb9527@foxmail.com>
2024-12-09 17:56:46 +0800

    添加员工接口

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -15,13 +17,26 @@ public class GlobalExceptionHandler {
 
     /**
      * 捕获业务异常
+     *
      * @param ex
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex) {
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException e) {
+        log.error("异常信息：{}", e.getMessage(), e);
+        if (e.getMessage().contains("Duplicate entry")) {
+            String[] strings = e.getMessage().split(" ");
+            return Result.error(strings[2] + "重复");
+        } else {
+            return Result.error(e.getMessage());
+        }
+
     }
 
 }
