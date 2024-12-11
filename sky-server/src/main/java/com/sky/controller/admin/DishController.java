fb6fa6eeab245ee80ff0617b63b6352a5a206bcb
xxb <xxb9527@foxmail.com>
2024-12-11 16:40:05 +0800

    菜品查询功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,11 +1,14 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +27,13 @@ public class DishController {
     public Result save(@RequestBody DishDTO dishDTO){
         dishService.saveDishFlavor(dishDTO);
         return Result.success();
+    }
+
+    @ApiOperation("菜品分页查询")
+    @GetMapping("/page")
+    public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+        PageResult pageResult = dishService.page(dishPageQueryDTO);
+        return Result.success(pageResult);
     }
 
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 
 public interface DishService {
 
     public void saveDishFlavor(DishDTO dishDTO);
 
+    PageResult page(DishPageQueryDTO dishPageQueryDTO);
 }
